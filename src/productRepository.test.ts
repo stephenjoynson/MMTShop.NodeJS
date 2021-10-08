c6ee6a66bb150c97c08cba1b5e80364588b4f022
Stephen Joynson <stephen.joynson@mmtdigital.co.uk>
2021-10-08 13:34:26 +0100

    Added more tests

---
+++ src/productRepository.test.ts
@@ -0,0 +1,37 @@
+import { Product } from "./product";
+import {
+  getFeaturedProducts,
+  getProductsByCategory,
+} from "./productRepository";
+import { products } from "../data/products.json";
+
+const featuredProducts = getFeaturedProducts();
+const productsByCategory = getProductsByCategory(1);
+
+test("getFeaturedProducts returns three Products", () => {
+  expect(featuredProducts.length).toBe(3);
+});
+
+test("getFeaturedProducts returns expected Products", () => {
+  expect(featuredProducts[0].sku).toBe(111111);
+  expect(featuredProducts[1].sku).toBe(222222);
+  expect(featuredProducts[2].sku).toBe(333333);
+});
+
+test("getFeaturedProducts returns expected property types for Products", () => {
+  const firstProduct = featuredProducts[0];
+  expect(firstProduct).toMatchObject<Product>(products[0]);
+});
+
+test("getProductsByCategory returns one Product", () => {
+  expect(productsByCategory.length).toBe(1);
+});
+
+test("getProductsByCategory returns expected Product", () => {
+  expect(productsByCategory[0].sku).toBe(111111);
+});
+
+test("getProductsByCategory returns expected property types for Products", () => {
+  const firstProduct = productsByCategory[0];
+  expect(firstProduct).toMatchObject<Product>(products[0]);
+});

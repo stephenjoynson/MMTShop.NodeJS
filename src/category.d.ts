d9234dca362f7147d1f554afc8a08823bbcdb804
Stephen Joynson <stephen.joynson@mmtdigital.co.uk>
2021-10-08 06:45:03 +0100

    Added some type definitions

---
+++ src/category.d.ts
@@ -0,0 +1,4 @@
+export interface Catgeory {
+  categoryId: bigint;
+  categoryName: string;
+}
---
+++ src/product.d.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  sku: bigint;
+  name: string;
+  description: string;
+  price: number;
+  categoryId: bigint;
+}

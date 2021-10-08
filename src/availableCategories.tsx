7173a72b9f72c01110e45b68f2ed7bee01ff6d70
Stephen Joynson <stephen.joynson@mmtdigital.co.uk>
2021-10-08 08:48:50 +0100

    Added some scripts

--- src/availableCategories.tsx
+++ src/availableCategories.tsx
@@ -2,8 +2,7 @@ import { categories } from "../data/categories.json";
 import { CategoryList } from "./categoryList";
 
 export const availableCategories = (): CategoryList => {
-
   return {
-    categoryList: categories.map(item => new CategoryList(item));
-  };
+    categoryList: categories.map(category =>
+  }; 
 };

17f3d4343a1492a8211438284441cd6b5c5cd6d7
Stephen Joynson <stephen.joynson@mmtdigital.co.uk>
2021-10-13 10:12:50 +0100

    Removed original comments. Reordered and removed whitespace.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
-/**
- * Module dependencies.
- */
-
 import express from "express";
 import { getAvailableCategories } from "../src/categoryRepository";
 import {
@@ -10,69 +6,38 @@ import {
 } from "../src/productRepository";
 
 const app = express();
+const apiKeys = ["foo", "bar", "baz"];
+const apipathversion = "/api/v1";
 
-// if we wanted to supply more than JSON, we could
-// use something similar to the content-negotiation
-// example.
-
-// here we validate the API key,
-// by mounting this middleware to /api
-// meaning only paths prefixed with "/api"
-// will cause this middleware to be invoked
+const featuredProducts = getFeaturedProducts();
+const categories = getAvailableCategories();
+const productsByCategory = (categoryId: number) => {
+  return getProductsByCategory(categoryId);
+};
 
 app.use("/api", function (req, res, next) {
   const key = req.query["api-key"]?.toString();
-
-  // key isn't present
   if (!key) {
     const err = new Error("API Key Required");
     res.status(400);
     next(err);
   }
-
-  // key is invalid
   if (!~apiKeys.indexOf(key)) {
     const err = new Error("API Key Invalid");
     res.status(401);
     next(err);
   }
-
   next();
 });
 
-// map of valid api keys, typically mapped to
-// account info with some sort of database like redis.
-// api keys do _not_ serve as authentication, merely to
-// track API usage or help prevent malicious behavior etc.
-
-const apiKeys = ["foo", "bar", "baz"];
-
-// these two objects will serve as our faux database
-
-const featuredProducts = getFeaturedProducts();
-
-const categories = getAvailableCategories();
-
-const productsByCategory = (categoryId: number) => {
-  return getProductsByCategory(categoryId);
-};
-
-const apipathversion = "/api/v1";
-
-// we now can assume the api key is valid,
-// and simply expose the data
-
-// example: http://localhost:3000/api/categories/?api-key=foo
 app.get(`${apipathversion}/categories`, function (req, res, next) {
   res.send(categories);
 });
 
-// example: http://localhost:3000/api/featuredproducts/?api-key=foo
 app.get(`${apipathversion}/featuredproducts`, function (req, res, next) {
   res.send(featuredProducts);
 });
 
-// example: http://localhost:3000/api/categories/1/products/?api-key=foo
 app.get(
   `${apipathversion}/categories/:categoryid/products`,
   function (req, res, next) {
